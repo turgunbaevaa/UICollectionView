0e4929e293a4f74ab36e5f40e496ea3617a1c671
turgunbaevaa <nyc.aruu@gmail.com>
2024-02-04 17:22:26 +0100

    Initial Commit

---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Aruuke Turgunbaeva on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
